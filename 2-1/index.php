aea2910df696d52530833aae620ee50699526dfc
nagoyayuto <nagoya.yuto.0925@gmail.com>
2020-08-27 15:26:26 +0900

    2-1課題提出
    index.phpの作成

---
+++ 2-1/index.php
@@ -0,0 +1,26 @@
+<?php
+// PHPの研修カリキュラム2-1
+?>
+<?php
+/*
+PHPの
+研修
+カリキュラム
+2-1
+*/
+?>
+<html>
+    <p>I say that <?php echo "Hello World!!"; ?></p>
+</html>
+<?php
+echo "1.開発環境・検証環境・本番環境"."<br>";
+echo "開発環境：ソフトウェアを開発するための環境のこと。様々なツールの集合からなる。"."<br>";
+echo "検証環境：開発し終わったソフトウェアの機能をテストするための環境のこと。"."<br>";
+echo "本番環境：システムが製品として実際に稼働している環境のこと。"."<br>";
+echo "2.基本設計書・詳細設計書"."<br>";
+echo "基本設計書：顧客に見える部分の設計書。例えばシステム全体の概要や大雑把な機能の一覧、作業スケジュールなど。"."<br>";
+echo "詳細設計書：顧客に見えない部分の設計書。"."<br>";
+echo "3.Ajax"."<br>";
+echo "Ajax：ウェブブラウザ内で非同期通信を行いながらインターフェースの構築を行うプログラミング手法である。"."<br>";
+?>
+<img class="alignnone size-full wp-image-371" src="http://54.238.147.46/wp-content/uploads/2020/04/update_task_status.png" alt="" width="716" height="180" data-pagespeed-url-hash="608921261" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
